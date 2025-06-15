0087b4b5e6841e0bda94ed2fe31e109b4ee2950c
Sudeep <sudeep.ganta123@gmail.com>
2025-06-15 19:31:54 +0530

    added new class

---
+++ src/main/java/RahulshettyEcommerce/SeleniumTesting/NewTest.java
@@ -0,0 +1,12 @@
+package RahulshettyEcommerce.SeleniumTesting;
+
+public class NewTest {
+
+	public static void main(String[] args) {
+		
+		//Added new class
+		// TODO Auto-generated method stub
+
+	}
+
+}
